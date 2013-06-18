0424f1629be260e4794a8b987c04d7a437a9717a
brunothi14 <bru.3002@gmail.com>
2013-06-18 16:19:46 -0400

    recom

--- config/deploy/production.rb
+++
@@ -1,3 +0,0 @@
-server "extra.centrekubota.ca", :app, :web, :db, :primary => true
-set :deploy_to, "/var/www/rails/inventaire"
-set :branch, 'stable'
--- config/deploy/staging.rb
+++
@@ -1,4 +0,0 @@
-server "test-extra.centrekubota.ca", :app, :web, :db, :primary => true
-set :deploy_to, "/var/www/rails/inventaire_staging"
-set :branch, 'master'
-set :rails_env, "staging"
