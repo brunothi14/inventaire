786f80b578b052d591e441128351ad332de6e4df
brunothi14 <bru.3002@gmail.com>
2013-06-20 16:05:55 -0400

    Reparation des problemes de Aptana

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 class Ability
   include CanCan::Ability
   
   def initialize(user)
     can :read, :all
   end
-  
-  
-=======
-class Ability
-  include CanCan::Ability
-  
-  def initialize(user)
-    can :read, :all
-  end
-  
-  
->>>>>>> master
+
 end
