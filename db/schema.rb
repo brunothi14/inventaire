cc1bb68aa72d8b4e66ac06abde0e98a089efabdb
Bruno Thibodeau <bru.3002@gmail.com>
2013-07-11 08:42:18 -0400

    Petits changements

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -72,60 +71,3 @@ ActiveRecord::Schema.define(:version => 20130614211252) do
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130610124314) do
-
-  create_table "attachements", :force => true do |t|
-    t.string   "fonction"
-    t.string   "emplacement"
-    t.string   "stock"
-    t.string   "marque"
-    t.string   "modele"
-    t.string   "description"
-    t.decimal  "prix"
-    t.decimal  "bonus"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "equipements", :force => true do |t|
-    t.string   "emplacement"
-    t.string   "stock"
-    t.string   "description"
-    t.string   "modele"
-    t.decimal  "prix"
-    t.decimal  "bonus"
-    t.date     "arrivee"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "tracteur_neufs", :force => true do |t|
-    t.string   "emplacement"
-    t.string   "stock"
-    t.string   "famille"
-    t.string   "modele"
-    t.string   "description"
-    t.decimal  "prix"
-    t.decimal  "bonus"
-    t.date     "arrivee"
-    t.integer  "facture"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-end
->>>>>>> master
