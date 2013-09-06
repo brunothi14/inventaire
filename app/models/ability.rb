0266e7d0eb3936c70c9c738237dd60d3f581406b
brunothi14 <bru.3002@gmail.com>
2013-09-06 13:24:44 -0400

    raparation du chemin dacces de la db sqlite prodcution

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,8 +1,22 @@
 class Ability
   include CanCan::Ability
-  
+ 
   def initialize(user)
-    can :read, :all
+    user ||= User.new # guest user
+ 
+    if user.role? :super_admin
+      can :manage, :all
+    elsif user.role? :product_admin
+      can :manage, [Tractor, Attachement, Equipement]
+    elsif user.role? :product_team
+      can :read, :all
+      # manage products, assets he owns
+      #can :manage, Product do |product|
+      #  product.try(:owner) == user
+      #end
+      #can :manage, Asset do |asset|
+      #  asset.assetable.try(:owner) == user
+      end
+    end
   end
-
 end
