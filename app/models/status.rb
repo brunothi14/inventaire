688f6d036fe0a99fa7e524fb7f155ce048ee2f42
brunothi14 <bthibodeau@alaporte.ca>
2013-10-07 15:14:55 -0400

    Correction express du status des usages

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,8 @@
 class Status < ActiveRecord::Base
   has_many :tractors
   has_many :snowblowers
+  has_many :useds
+  has_many :used_others
+  has_many :used_accessories
   attr_accessible :stat
 end
